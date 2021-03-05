31e9ceffaefb94773a0e309ab86aea5b3989186b
Dusit Prasertsilp <dusit.prasertsilp@hotmail.com>
2021-03-05 12:39:47 +0700

    Setup for Heroku

--- RestAPI.Address/Program.cs
+++ RestAPI.Address/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace RestAPI.Address
 {
@@ -13,7 +9,16 @@ namespace RestAPI.Address
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseConfiguration(config)
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
